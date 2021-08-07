12ea7a0263b47c0742721c95861937d3a25ef422
Aravind Kumar <84002440+Aravindtetradev@users.noreply.github.com>
2021-08-07 20:49:33 +0530

    Create CodeforData.java

---
+++ CodeforData.java
@@ -0,0 +1,9 @@
+package com.purpleborecode;                     //Change package name
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Date date=new Date();                   //instance of date class
+        date.getTime();                         //getTime member
+        System.out.println(date);
+    }
+}
